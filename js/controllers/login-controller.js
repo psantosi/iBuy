d32825531c8a131471b2625b2b58a877b3959700
psantosi <paty.santos18@hotmail.com>
2017-08-31 16:55:48 -0300

    bug fix

--- js/controllers/login-controller.js
+++ js/controllers/login-controller.js
@@ -1,1 +1,1 @@
-app.controller("LoginController",function(o,n){o.logar=function(){"admin"===o.login&&"admin"===o.senha?n.url("/produtos"):console.log("não")}});
+app.controller("LoginController",function(e,n,o){e.logar=function(){"admin"===e.login&&"admin"===e.senha?n.url("/produtos"):(e.mensagem="Usuario ou senha incorreta",o.open({template:"/templates/mensagem.html",className:"ngdialog-theme-default mensagem-box",scope:e}))}});
